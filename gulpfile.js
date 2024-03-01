b27689851cbf5ccf1ba3cafa811bc0ee238cca59
Nasuha Asri <NASAS@orsted.com>
2024-03-01 16:56:02 +0800

    lesson 5: project structure
    - shinobi/**/*.scss: looking for any scss files in subfolder also
    - order of import is important

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ const sass = require('gulp-sass')(require('sass'));
 
 // compiling sass to css file and put it in css root folder
 function buildStyles() {
-    return src('*.scss')
+    return src('shinobi/**/*.scss')
         .pipe(sass())
         .pipe(dest('css'))
 }
 
 // watch source sass file - auto run buildStyles() when we making changes
 function watchTask() {
-    watch(['*.scss'], buildStyles)
+    watch(['shinobi/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask);
