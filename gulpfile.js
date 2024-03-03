15c4b6126fe9672e9361b0f91c97d58f6842301b
Nasuha Asri <NASAS@orsted.com>
2024-03-03 16:44:09 +0800

    lesson 20: purge css
    - making css files smaller by removing any unused classes and rules using purgecss

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,21 @@ const { src, dest, watch, series } = require('gulp');
 // invoke sass compiler and it will return a function
 // sass - compile our sass file
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 // compiling sass to css file and put it in css root folder
 function buildStyles() {
     return src('shinobi/**/*.scss')
         .pipe(sass())
+        // look into html files and see what classes and rules do we actually use
+        // look at our css files and take out any rules we dont use
+        .pipe(purgecss({ content: ['*.html'] }))
         .pipe(dest('css'))
 }
 
 // watch source sass file - auto run buildStyles() when we making changes
 function watchTask() {
-    watch(['shinobi/**/*.scss'], buildStyles)
+    watch(['shinobi/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask);
